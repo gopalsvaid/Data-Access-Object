b6cbbc6886fdc41ac372f8743e56c50a715eea7d
Gopal Vaid <gopalvaid@ravis-mac-mini.local>
2016-04-15 10:27:36 +0530

    Initial Commit

---
+++ DaoTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DaoTest
+//
+//  Created by Gopal.Vaid on 15/04/16.
+//  Copyright © 2016 Gopal.Vaid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
